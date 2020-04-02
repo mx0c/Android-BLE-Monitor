3788318d9eb5230909587b1c4671d90aa32693a7
mx0c <marius-bauer-2006@web.de>
2020-04-02 23:29:42 +0200

    added listview and custom adapter

---
+++ app/src/main/java/com/huc/android_ble_monitor/DeviceArrayAdapter.java
@@ -0,0 +1,35 @@
+package com.huc.android_ble_monitor;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class DeviceArrayAdapter extends ArrayAdapter<BluetoothDevice> {
+    public DeviceArrayAdapter(Context context, List<BluetoothDevice> devices) {
+        super(context, 0, devices);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        BluetoothDevice dev = getItem(position);
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.device_list_item, parent, false);
+        }
+
+        TextView tvName = convertView.findViewById(R.id.textView_name);
+        TextView tvAddress = convertView.findViewById(R.id.textView_address);
+        TextView tvBonded = convertView.findViewById(R.id.textView_bonded);
+
+        tvName.setText("Name: " + dev.getName());
+        tvAddress.setText("Address: " + dev.getAddress());
+        tvBonded.setText("Bonded: " + dev.getBondState());
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/huc/android_ble_monitor/MainActivity.java
+++ app/src/main/java/com/huc/android_ble_monitor/MainActivity.java
@@ -1,7 +1,10 @@
 package com.huc.android_ble_monitor;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -9,45 +12,70 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
-public class DeviceScanActivity extends ListActivity {
+public class MainActivity extends Activity {
 
     private static final int REQUEST_ENABLE_BT = 0;
     // Stops scanning after 10 seconds.
-    private static final long SCAN_PERIOD = 10000;
+    private static final long SCAN_PERIOD = 30000;
 
     private BluetoothAdapter mBluetoothAdapter;
-    private boolean mScanning;
-    private Handler mHandler;
-    private List<BluetoothDevice> mDeviceList;
+    private boolean mScanning = false;
+    private Handler mHandler = new Handler();
+    private List<BluetoothDevice> mDeviceList = new ArrayList<BluetoothDevice>();
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mListView = findViewById(R.id.deviceList);
+
+        DeviceArrayAdapter devAdapter = new DeviceArrayAdapter(this, mDeviceList);
+        mListView.setAdapter(devAdapter);
+
         checkBleAvailability();
-        checkForBluetoothEnabled();
+        if(checkForBluetoothEnabled()){
+            scanBleDevices(true);
+        }
     }
 
-    void checkBleAvailability(){
+    private void checkBleAvailability(){
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
             finish();
         }
     }
 
-    void checkForBluetoothEnabled(){
+    private boolean checkForBluetoothEnabled(){
         this.mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (this.mBluetoothAdapter == null || !this.mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            // if bluetooth enabled
+            scanBleDevices(true);
         }
     }
 
-    void scanBleDevices(final boolean enable){
+    private void scanBleDevices(final boolean enable){
         if(enable){
             mHandler.postDelayed(new Runnable() {
                 @Override
