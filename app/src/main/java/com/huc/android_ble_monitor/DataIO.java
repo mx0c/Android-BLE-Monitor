37eaa482dd3f09589ae4b5fa3be1f9edb5edfcae
mx0c <marius-bauer-2006@web.de>
2020-04-18 13:54:47 +0200

    added methods to load service and characteristic json arrays

--- app/src/main/java/com/huc/android_ble_monitor/DataIO.java
+++ app/src/main/java/com/huc/android_ble_monitor/DataIO.java
@@ -45,43 +45,45 @@ public class DataIO {
                 ex.printStackTrace();
             }
         }
-
         return map;
     }
 
-    //from https://github.com/tessel/bleadvertise/blob/master/lib/packet.js
-    public static List<Pair<Integer, String>> ADTypes = Arrays.asList(
-            new Pair<Integer, String>(0x01, "Flags"),
-            new Pair<Integer, String>(0x02, "Incomplete List of 16-bit Service Class UUIDs"),
-            new Pair<Integer, String>(0x03, "Complete List of 16-bit Service Class UUIDs"),
-            new Pair<Integer, String>(0x04, "Incomplete List of 32-bit Service Class UUIDs"),
-            new Pair<Integer, String>(0x05, "Complete List of 32-bit Service Class UUIDs"),
-            new Pair<Integer, String>(0x06, "Incomplete List of 128-bit Service Class UUIDs"),
-            new Pair<Integer, String>(0x07, "Complete List of 128-bit Service Class UUIDs"),
-            new Pair<Integer, String>(0x08, "Shortened Local Name"),
-            new Pair<Integer, String>(0x09, "Complete Local Name"),
-            new Pair<Integer, String>(0x0A, "Tx Power Level"),
-            new Pair<Integer, String>(0x0D, "Class of Device"),
-            new Pair<Integer, String>(0x0E, "Simple Pairing Hash C"),
-            new Pair<Integer, String>(0x0F, "Simple Pairing Randomizer R"),
-            new Pair<Integer, String>(0x10, "Device ID"),
-            new Pair<Integer, String>(0x11, "Security Manager Out of Band Flags"),
-            new Pair<Integer, String>(0x12, "Slave Connection Interval Range"),
-            new Pair<Integer, String>(0x14, "List of 16-bit Service Solicitation UUIDs"),
-            new Pair<Integer, String>(0x1F, "List of 32-bit Service Solicitation UUIDs"),
-            new Pair<Integer, String>(0x15, "List of 128-bit Service Solicitation UUIDs"),
-            new Pair<Integer, String>(0x16, "Service Data"),
-            new Pair<Integer, String>(0x17, "Public Target Address"),
-            new Pair<Integer, String>(0x18, "Random Target Address"),
-            new Pair<Integer, String>(0x19, "Appearance"),
-            new Pair<Integer, String>(0x1A, "Advertising Interval"),
-            new Pair<Integer, String>(0x1B, "LE Bluetooth Device Address"),
-            new Pair<Integer, String>(0x1C, "LE Role"),
-            new Pair<Integer, String>(0x1D, "Simple Pairing Hash C-256"),
-            new Pair<Integer, String>(0x1E, "Simple Pairing Randomizer R-256"),
-            new Pair<Integer, String>(0x20, "Service Data - 32-bit UUID"),
-            new Pair<Integer, String>(0x21, "Service Data - 128-bit UUID"),
-            new Pair<Integer, String>(0x3D, "3D Information Data"),
-            new Pair<Integer, String>(0xFF, "Manufacturer Specific Data")
-    );
+    static public HashMap<String, HashMap<String,String>> loadCharacteristicData(Context ctx){
+        JSONArray jsonArray = loadJSONArrayFromAsset(ctx, "characteristic_uuids.json");
+        HashMap<String,HashMap<String,String>> map = new HashMap<>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+                JSONObject obj = jsonArray.getJSONObject(i);
+                String test = obj.keys().next();
+                HashMap<String,String> submap = new HashMap<String, String>();
+                submap.put("name",obj.getString("name"));
+                submap.put("identifier",obj.getString("identifier"));
+                map.put(obj.getString("uuid"), submap);
+            }catch (JSONException ex){
+                ex.printStackTrace();
+            }
+        }
+        return map;
+    }
+
+    static public HashMap<String, HashMap<String,String>> loadServiceData(Context ctx){
+        JSONArray jsonArray = loadJSONArrayFromAsset(ctx, "service_uuids.json");
+        HashMap<String,HashMap<String,String>> map = new HashMap<>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+                JSONObject obj = jsonArray.getJSONObject(i);
+                String test = obj.keys().next();
+                HashMap<String,String> submap = new HashMap<String, String>();
+                submap.put("name",obj.getString("name"));
+                submap.put("identifier",obj.getString("identifier"));
+                map.put(obj.getString("uuid"), submap);
+            }catch (JSONException ex){
+                ex.printStackTrace();
+            }
+        }
+        return map;
+    }
+
 }
