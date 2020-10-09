ab5756bd308a1996ef0ca19c66757efd6c906b7e
mx0c <marius-bauer-2006@web.de>
2020-10-09 14:02:30 +0200

    added appbar icons to ble log activity + change snoop log path dialog

--- app/src/main/java/com/huc/android_ble_monitor/dialogs/SetSnoopFilePathDialog.java
+++ app/src/main/java/com/huc/android_ble_monitor/dialogs/SetSnoopFilePathDialog.java
@@ -2,6 +2,8 @@ package com.huc.android_ble_monitor.dialogs;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -10,9 +12,10 @@ import com.huc.android_ble_monitor.R;
 import com.huc.android_ble_monitor.util.HciSnoopLogUtil;
 
 public class SetSnoopFilePathDialog extends AlertDialog {
+    final static String TAG = "BLEM_SoopFileDialog";
     private EditText mSnoopFileEditText;
 
-    protected SetSnoopFilePathDialog(Context context) {
+    public SetSnoopFilePathDialog(Context context) {
         super(context);
         LayoutInflater inflater = getLayoutInflater();
         View dialoglayout = inflater.inflate(R.layout.dialog_set_snoop_path, null);
@@ -21,7 +24,17 @@ public class SetSnoopFilePathDialog extends AlertDialog {
         mSnoopFileEditText = (EditText) dialoglayout.findViewById(R.id.snoop_file_edit);
         mSnoopFileEditText.setText(HciSnoopLogUtil.BTSNOOP_PATH);
         mSnoopFileEditText.setSelection(mSnoopFileEditText.getText().length());
+        setTitle("Select BTSnoop Log Path");
 
-        setTitle("Select BTSnoop Log Filepath");
+        setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // save selected path
+                HciSnoopLogUtil.BTSNOOP_PATH = mSnoopFileEditText.getText().toString();
+                Log.d(TAG, "onClick Positive Button: Saved BtSnoop Path " + HciSnoopLogUtil.BTSNOOP_PATH);
+            }
+        });
+
+        setButton(DialogInterface.BUTTON_NEGATIVE, "CANCEL", (DialogInterface.OnClickListener) null);
     }
 }
